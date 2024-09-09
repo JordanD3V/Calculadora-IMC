ccc4657a752f4a831cf79e104202d77080ff2505
Jordan Henrique <jordandev586@gmail.com>
2024-09-09 20:25:10 -0300

    lógica app

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -7,10 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnCalcular: Button
+
+    private lateinit var textInputPeso: TextInputLayout
+    private lateinit var editTextPeso: TextInputEditText
+
+    private lateinit var textInputAltura: TextInputLayout
+    private lateinit var editTextAltura: TextInputEditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +31,47 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        textInputPeso = findViewById(R.id.txt_input_peso)
+        textInputAltura = findViewById(R.id.txt_input_altura)
+
+        editTextAltura = findViewById(R.id.edit_txt_altura)
+        editTextPeso = findViewById(R.id.edit_txt_peso)
         btnCalcular = findViewById(R.id.btn_calcular)
+
         btnCalcular.setOnClickListener {
+            preparacaoCalculoImc()
+        }
+
+    }
+
+    private fun validarCampos(pacientePeso: String, pacienteAltura: String): Boolean{
+        textInputAltura.error = null
+        textInputPeso.error = null
+
+        if (pacientePeso.isEmpty()) {
+            textInputPeso.error = "Digite o seu peso"
+            return false
+        } else if (pacienteAltura.isEmpty()) {
+            textInputAltura.error = "Digite a sua altura"
+            return false
+        }
+        return true
+    }
+    private fun preparacaoCalculoImc() {
+
+        val altura = editTextAltura.text.toString()
+        val peso = editTextPeso.text.toString()
+
+        val resultadoValidacao = validarCampos(peso, altura)
+
+        if(resultadoValidacao){
             val intent = Intent(this, ResultsActivity::class.java)
+
+            if (peso.isNotEmpty() && altura.isNotEmpty()) {
+                intent.putExtra("peso", peso.toDouble())
+                intent.putExtra("altura", altura.toDouble())
+            }
+
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/calculadoraimc/ResultsActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/ResultsActivity.kt
@@ -1,12 +1,21 @@
 package com.example.calculadoraimc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultsActivity : AppCompatActivity() {
+
+    private lateinit var textInfoPeso: TextView
+    private lateinit var textInfoAltura: TextView
+    private lateinit var textInfoDiagnostico: TextView
+    private lateinit var buttonNovamente: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,7 +24,39 @@ class ResultsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
+
+        textInfoAltura = findViewById(R.id.txt_info_altura)
+        textInfoPeso = findViewById(R.id.txt_info_peso)
+        textInfoDiagnostico = findViewById(R.id.txt_diagnostico)
+        buttonNovamente = findViewById(R.id.novo_calculo)
+
+        val bundle = intent.extras
+
+        if(bundle != null){
+             val pesoPaciente = bundle?.getDouble("peso",0.0)?: 0.0
+             val alturaPaciente = bundle?.getDouble("altura",0.0)?: 0.0
+
+            textInfoPeso.text = "Peso informado: $pesoPaciente kg"
+            textInfoAltura.text = "Altura informada: $alturaPaciente m"
+
+            val imc = pesoPaciente / (alturaPaciente * alturaPaciente)
+
+            val resultado = when {
+                imc < 18.5 -> "Seu IMC indica Baixo peso"
+                imc in 18.5..24.9 -> "Seu IMC indica Normal"
+                imc in 25.0..29.9 -> "Seu IMC indica Sobrepeso"
+                else -> "Seu IMC indica Obesidade"
+            }
+
+            textInfoDiagnostico.text = resultado
+        }
+
+        buttonNovamente.setOnClickListener {
+            finish()
+        }
+
     }
+
+
 }
